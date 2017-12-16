e95a6fea7b3a10df411ad2e44e39509733005120
Nikola Bebic <bn140314d@student.etf.bg.ac.rs>
2017-12-16 18:40:18 +0100

    Update project structure

--- workspace/MJCompiler/test/rs/ac/bg/etf/pp1/MJTest.java
+++ workspace/MJCompiler/test/rs/ac/bg/etf/pp1/MJTest.java
@@ -25,7 +25,7 @@ public class MJTest {
 		Reader br = null;
 		try {
 			
-			File sourceCode = new File("program.mj");
+			File sourceCode = new File("MJCompiler/test/program.mj");
 			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
 			
 			br = new BufferedReader(new FileReader(sourceCode));
